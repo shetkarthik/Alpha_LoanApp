1ade2cd1db3a2d94d4f57bb05a311e44cfa41033
“Karthik” <shetkarthik89@gmail.com>
2023-06-18 11:51:40 +0530

    commit on 18/06

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/client/login/login.component';
+import { SignupComponent } from './components/client/signup/signup.component';
+import { HomeComponent } from './components/client/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
-import { LoaneligibleComponent } from './components/loaneligible/loaneligible.component';
-import { AuthlogComponent } from './components/authlog/authlog.component';
-import { LoandetailsComponent } from './components/loandetails/loandetails.component';
-import { LoanFormComponent } from './components/loan-form/loan-form.component';
-import { LoancaltrComponent } from './components/loancaltr/loancaltr.component';
-import { LoantypeComponent } from './components/loantype/loantype.component';
+import { LoaneligibleComponent } from './components/client/loaneligible/loaneligible.component';
+import { AuthlogComponent } from './components/client/authlog/authlog.component';
+import { LoandetailsComponent } from './components/client/loandetails/loandetails.component';
+import { LoanFormComponent } from './components/client/loan-form/loan-form.component';
+import { LoancaltrComponent } from './components/client/loancaltr/loancaltr.component';
+import { LoantypeComponent } from './components/client/loantype/loantype.component';
 import { RouteGuardGuard } from './guards/route-guard.guard';
 
 
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/client/login/login.component';
+import { SignupComponent } from './components/client/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgToastModule } from 'ng-angular-popup';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/client/home/home.component';
 import { TokenInterceptor } from './interceptors/token.interceptor';
-import { LoaneligibleComponent } from './components/loaneligible/loaneligible.component';
-import { AuthlogComponent } from './components/authlog/authlog.component';
+import { LoaneligibleComponent } from './components/client/loaneligible/loaneligible.component';
+import { AuthlogComponent } from './components/client/authlog/authlog.component';
 import { NgxLoadingModule } from "ngx-loading";
-import { LoandetailsComponent } from './components/loandetails/loandetails.component';
-import { LoanFormComponent } from './components/loan-form/loan-form.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoancaltrComponent } from './components/loancaltr/loancaltr.component';
-import { LoantypeComponent } from './components/loantype/loantype.component';
+import { LoandetailsComponent } from './components/client/loandetails/loandetails.component';
+import { LoanFormComponent } from './components/client/loan-form/loan-form.component';
+import { NavbarComponent } from './components/client/navbar/navbar.component';
+import { LoancaltrComponent } from './components/client/loancaltr/loancaltr.component';
+import { LoantypeComponent } from './components/client/loantype/loantype.component';
 
 
 
